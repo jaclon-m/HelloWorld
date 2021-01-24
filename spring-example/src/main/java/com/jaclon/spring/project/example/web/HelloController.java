a22005dfd1923101d04ab74166a971f0c24f412a
jaclon-m <jaclon_m@163.com>
2021-01-24 10:59:12 +0800

    1

--- spring-example/src/main/java/com/jaclon/spring/project/example/web/HelloController.java
+++ spring-example/src/main/java/com/jaclon/spring/project/example/web/HelloController.java
@@ -1,21 +1,28 @@
 package com.jaclon.spring.project.example.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * TODO Description
  * @author jaclon
  * @since 2020/12/17 下午6:49
  */
-@Controller
+@RestController
 public class HelloController {
 
-  @RequestMapping("/hello")
-  @ResponseBody
+  @GetMapping("/hello")
   public String HelloUrl(){
     System.out.println("=============Hello World==============");
     return "Hello World";
+  }
+
+  @RequestMapping("/helloMVC")
+  public String HelloMVC(){
+    System.out.println("=============Hello World==============");
+    return "Hello World2";
   }
 }
