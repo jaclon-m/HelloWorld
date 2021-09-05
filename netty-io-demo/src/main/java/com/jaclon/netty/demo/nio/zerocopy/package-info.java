7607afc7e11f8b35c1d2eac22fc94dab94f845ee
jaclon-m <jaclon_m@163.com>
2021-09-05 21:26:00 +0800

    java IO

---
+++ netty-io-demo/src/main/java/com/jaclon/netty/demo/nio/zerocopy/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * 代码这次先忽略
+ *  java实现零拷贝有两种方式，mmap和sendFile.其中sendFile比mmap效率更高
+ * TODO mmap了解
+ *
+ * @author jaclon
+ * @since 2021/9/5 21:23
+ */
+package com.jaclon.netty.demo.nio.zerocopy;
