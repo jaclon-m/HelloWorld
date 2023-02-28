3003081efc78cd6c0b61e8aa85460604c22e6c76
jaclon-m <jaclon_m@163.com>
2023-02-28 14:55:23 +0800

    rename& rabbitmq demo

---
+++ rabbitmq-example-consumer/src/main/java/com/jaclon/config/RabbitMQConfig.java
@@ -0,0 +1,9 @@
+package com.jaclon.config;
+
+public class RabbitMQConfig {
+    public static final String RABBITMQ_DEMO_TOPIC = "rabbitmq-demo-topic";
+
+    public static final String RABBITMQ_DEMO_DIRECT_EXCHANGE="rabbitmq-demo-direct-exchange";
+
+    public static final String RABBITMQ_DEMO_DIRECT_ROUTING = "rabbitmq-demo-direct-routing";
+}
---
+++ rabbitmq-example-productor/README.md
@@ -0,0 +1,2 @@
+https://developer.aliyun.com/article/769883#slide-15
+消息追踪 https://www.cnblogs.com/colin88/p/15734981.html
