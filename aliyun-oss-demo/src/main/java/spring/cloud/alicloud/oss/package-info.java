22823437ed938511de6ae02879617a45779ffab2
jaclon-m <jaclon_m@163.com>
2024-05-24 15:37:35 +0800

    包问题处理

--- aliyun-oss-demo/src/main/java/spring/cloud/alicloud/oss/package-info.java
+++ aliyun-oss-demo/src/main/java/spring/cloud/alicloud/oss/package-info.java
@@ -1,8 +1,6 @@
 /**
  *
- * 用户登录名称 oss-test@1699103044723987.onaliyun.com
- * AccessKey ID LTAI5tDf54jNc5QnmQ1mASJp
- * AccessKey Secret FRoNz4Is7Jn8rkJsxVHHmqe5FNo5eM
+
  * @author jaclon
  * @since 2024/3/18 10:25
  */
